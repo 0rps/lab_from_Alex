33ff13a536e9819a79b7e3dbadbbc1e26ed12eb6
Uliana_S <2791785@mail.ru>
2020-07-25 15:05:26 +0300

    Changing the output of new data

--- school_42/d02/ex05_ft_print_comb2.py
+++ school_42/d02/ex05_ft_print_comb2.py
@@ -8,16 +8,15 @@ def print_comb():
             number = str(i) + str(j)
             all_numbers.append(number)
 
-    pairs_of_numbers = []
+    flag = False
     for i in range(len(all_numbers)-1):
-        for j in range(i+1,len(all_numbers)):
-            pairs_of_numbers.append(tuple((all_numbers[i], all_numbers[j])))
+        for j in range(i+1, len(all_numbers)):
+            if flag:
+                print(", " + all_numbers[i] + " " + all_numbers[j], end = "")
+            else:
+                print(all_numbers[i] + " " + all_numbers[j], end="")
+                flag = True
 
-    for i in range(len(pairs_of_numbers)):
-        if i == len(pairs_of_numbers)-1:
-            print(f'{pairs_of_numbers[i][0]} {pairs_of_numbers[i][1]}')
-        else:
-            print(f'{pairs_of_numbers[i][0]} {pairs_of_numbers[i][1]}', end=', ')
 
 if __name__ == "__main__":
     print_comb()
