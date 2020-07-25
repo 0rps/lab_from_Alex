2151c85a35e1579c2d792a9713c8d90862cd90df
Uliana_S <2791785@mail.ru>
2020-07-25 14:47:30 +0300

    Optimization of the algorithm and changing the output of new data

--- school_42/d02/ex04_ft_print_comb.py
+++ school_42/d02/ex04_ft_print_comb.py
@@ -2,31 +2,18 @@
 # listed by ascending order - yes, repetition is voluntary.
 
 def print_comb():
-    result = []
     number = ''
+    flag = False
+
     for i in range(10):
         for k in range(i+1, 10):
             for l in range(k+1, 10):
                 number = str(i) + str(k) + str(l)
-                if len(result) == 0:
-                    result.append(number)
+                if flag:
+                    print(', ' + str(number), end = "")
                 else:
-                    j = 0
-                    flag = False
-                    while j < len(result):
-                        number_j = result[j]
-                        if str(i) in number_j and str(k) in number_j and str(l) in number_j:
-                            flag = True
-                            break
-                        j += 1
-                    if flag == False:
-                        result.append(number)
-
-    for i in range(len(result)):
-        if i == len(result)-1:
-            print(result[i])
-        else:
-            print(result[i], end=', ')
+                    print(number, end = '')
+                    flag = True
 
 
 if __name__ == "__main__":
