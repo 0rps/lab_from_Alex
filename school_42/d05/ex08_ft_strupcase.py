046781af7d7b4fb1719e6a9438628dbf130f40fc
Uliana_S <2791785@mail.ru>
2020-07-26 22:49:50 +0300

    Fixed magic number in task ex08

--- school_42/d05/ex08_ft_strupcase.py
+++ school_42/d05/ex08_ft_strupcase.py
@@ -1,15 +1,18 @@
 # Create a function that transforms every letter of every word to uppercase.
 
+
 def create_uppercase(data_str):
     upper_str = ""
+    delta_between_big_and_little_letters = ord("a") - ord("A")
     for i in data_str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            symbol = chr(ord(i) - 32)
+        if ord("a") <= ord(i) <= ord("z"):
+            symbol = chr(ord(i) - delta_between_big_and_little_letters)
         else:
             symbol = i
         upper_str += symbol
 
     return upper_str
+
 
 def main():
     while True:
@@ -20,6 +23,7 @@ def main():
         break
 
     print(create_uppercase(data))
+
 
 if __name__ == "__main__":
     main()
