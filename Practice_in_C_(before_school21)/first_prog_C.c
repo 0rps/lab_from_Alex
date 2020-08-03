2c008771b7aa7a74be33e586f086f68f832d8996
Uliana Sozinova <2791785@mail.ru>
2020-08-04 00:23:41 +0300

    Added line output

--- Practice_in_C_(before_school21)/first_prog_C.c
+++ Practice_in_C_(before_school21)/first_prog_C.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
 
 int main(void){
 	int data1, data2;
-	char* str;
-	malloc // прочитать !!!!
+	char *string;
+	string = malloc(_MAX_PATH);
+	
 	printf("Enter number: ");
-	scanf("%d", &data1);
-	printf("Your number %d", &data1);
+	scanf("%d", data1);
+	printf("Your number: %d", data1);
+	
 	printf("Enter dotted number: ");
-	scanf("%f", &data2);
-	printf("Your number %.f2", &data2);
+	scanf("%f", data2);
+	printf("Your number: %.2f", data2);
+	
 	printf("Enter string: ");
-	scanf("%s", &str);
-	printf("Your string %s", &str);
+	gets(string);
+	printf("Your string: %s", string);
+	
+	free(string);
 	return 0;
 }
