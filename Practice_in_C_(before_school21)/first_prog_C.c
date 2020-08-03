5efbd2d7a6b9f489349c90915f89273c8265801f
Uliana_S <2791785@mail.ru>
2020-08-03 11:50:51 +0300

    First program in C

---
+++ Practice_in_C_(before_school21)/first_prog_C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+	int data1, data2;
+	char* str;
+	malloc // прочитать !!!!
+	printf("Enter number: ");
+	scanf("%d", &data1);
+	printf("Your number %d", &data1);
+	printf("Enter dotted number: ");
+	scanf("%f", &data2);
+	printf("Your number %.f2", &data2);
+	printf("Enter string: ");
+	scanf("%s", &str);
+	printf("Your string %s", &str);
+	return 0;
+}
