5362aa0ee17abad072dea3d1f8693893cc5d342a
Uliana Sozinova <2791785@mail.ru>
2020-08-04 16:28:17 +0300

    Fixed line reading

--- Practice_in_C_(before_school21)/first_prog_C.c
+++ Practice_in_C_(before_school21)/first_prog_C.c
@@ -3,18 +3,25 @@
 #include <stdlib.h>
 
 int main(void){
-	int data1, data2;
+	int data1;
+	float data2;
 	char *string;
-	string = malloc(_MAX_PATH);
+	string = (char*)malloc(_MAX_PATH);
 	
 	printf("Enter number: ");
-	scanf("%d", data1);
-	printf("Your number: %d", data1);
+	scanf("%d", &data1);
+	printf("Your number: %d\n", data1);
 	
 	printf("Enter dotted number: ");
-	scanf("%f", data2);
-	printf("Your number: %.2f", data2);
+	scanf("%f", &data2);
+	printf("Your number: %.2f\n", data2);
 	
+	char c = getc(stdin);
+	while(c!=10)
+	{
+		c = getc(stdin);
+	};
+
 	printf("Enter string: ");
 	gets(string);
 	printf("Your string: %s", string);
